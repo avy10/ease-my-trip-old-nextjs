d711913c5f65dcc410b3146fd635b08cf557bce9
Abhishek Kumar <avykr07@gmail.com>
2024-05-01 19:00:06 +0530

    fixing vercel deployement error 2nd attempt

--- pages/flights/index.jsx
+++ pages/flights/index.jsx
@@ -1,6 +1,5 @@
 import FlightHome from "@/components/Flights/FlightsHome";
 import { FlightSearchProvider } from "@/contexts/FlightSearchContext";
-import App from "@/pages/flights/_app";
 export default function Home() {
 	/* return (
 		<App>
